8e6709b3a85c870ffa1223e3bcf8d8c5434e129f
luogantt <luoganttccqy@gmail.com>
2025-08-11 12:09:24 +0800

    galactic

---
+++ keyoard_control.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+source /home/orangepi/scout_ros2_humble_lg/install/setup.sh
+
+
+ros2 run teleop_twist_keyboard teleop_twist_keyboard
---
+++ start.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+source /home/orangepi/scout_ros2_humble_lg/install/setup.sh
+
+ros2 launch scout_base scout_base.launch.py
